3c325e52586573a8d8a8008e7407c2084286de0a
alicia <aliciafdezrov@gmail.com>
2022-01-31 20:53:05 +0100

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {LoginComponent} from "./login/login.component";
+import {CrudComponent} from "./crud/crud.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {GalleryComponent} from "./gallery/gallery.component";
+import {ProfileComponent} from "./profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'crud',
+    component: CrudComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CrudComponent } from './crud/crud.component';
+import { ProfileComponent } from './profile/profile.component';
+import {MenuComponent} from "./layout/menu/menu.component";
+import {MaterialExampleModule} from './material.module';
+import {HeaderComponent} from "./layout/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    DashboardComponent,
+    GalleryComponent,
+    CrudComponent,
+    ProfileComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
