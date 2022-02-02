67b7c6e69613271dd3ee949068446da926a15d4c
alicia <aliciafdezrov@gmail.com>
2022-02-02 23:18:42 +0100

    Use FormGroup to control the login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ValidationErrors} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from "../services/auth.service";
-import {UserEntity} from "../model/userEntity";
 import {Router} from "@angular/router";
 
 @Component({
@@ -10,44 +9,40 @@ import {Router} from "@angular/router";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public user: UserEntity;
+  public loginForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) {
-    this.user = {
-      email: '',
-      password: '',
-    }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
+    });
   }
 
   ngOnInit(): void {
   }
 
-  getEmailErrorMessage(emailErrors: ValidationErrors | null): string {
-    if(!emailErrors) return '';
-
-    if (emailErrors["required"]) {
+  getEmailErrorMessage(): string {
+    if (this.loginForm.get("email")?.hasError("required")) {
       return 'You must enter a value';
     }
 
-    if (emailErrors["pattern"]) {
+    if (this.loginForm.get("email")?.hasError("pattern")) {
       return "Incorrect email. Try: master8@lemoncode.net"
     }
 
     return 'Not a valid email';
   }
 
-  getPasswordErrorMessage(passwordErrors: ValidationErrors | null): string {
-    if(!passwordErrors) return '';
-
-    if (passwordErrors["required"]) {
+  getPasswordErrorMessage(): string {
+    if (this.loginForm.get('password')?.hasError("required")) {
       return 'You must enter a value';
     }
 
-    if (passwordErrors["minlength"]) {
+    if (this.loginForm.get('password')?.hasError("minlength")) {
       return 'Your password cannot have less than 8 characters';
     }
 
-    if (passwordErrors["pattern"]) {
+    if (this.loginForm.get('password')?.hasError("pattern")) {
       return 'Your password only can container numbers';
     }
 
@@ -55,7 +50,7 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    const isValidAuthentication = this.authService.login(this.user);
+    const isValidAuthentication = this.authService.login(this.loginForm.value);
 
     if(isValidAuthentication) {
       this.router.navigate(['/dashboard']);
